c3785b6103ffc2e2aee634560df86f2c0393c6fa
Khizar <khizarnawab1@outlook.com>
2026-02-03 21:49:45 +0500

    refactor(components): remove HireMe component
    Remove unused HireMe component and update exports

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,6 +2,5 @@ import Navbar from "./Navbar";
 import Header from "./Header";
 import Transition from "./Transition";
 import Footer from "./Footer";
-import HireMe from "./HireMe";
 
-export { Navbar, Header, Transition, Footer, HireMe };
+export { Navbar, Header, Transition, Footer };
