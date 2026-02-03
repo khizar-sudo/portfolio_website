8a08ca1395c60a463041642154e9ac1410f6eede
Khizar <khizarnawab1@outlook.com>
2026-02-03 21:49:34 +0500

    refactor(assets): reorganize into subdirectories
    Move assets into organized structure:
    
    - branding/ for logos
    
    - icons/navigation/ for nav icons
    
    - icons/social/ for social media icons
    
    - icons/tech/ for technology icons
    
    - icons/ui/ for UI elements
    
    - images/ for profile and project images
    
    Remove unused assets: expressjs, framer-motion, text icons, work icons

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,94 +1,92 @@
-import home from "./home.svg";
-import about from "./about.svg";
-import work from "./work.svg";
-import projects from "./projects.svg";
-import contact from "./contact.svg";
+// Navigation Icons
+import home from "./icons/navigation/home.svg";
+import homeWhite from "./icons/navigation/home-white.svg";
+import about from "./icons/navigation/about.svg";
+import aboutWhite from "./icons/navigation/about-white.svg";
+import projects from "./icons/navigation/projects.svg";
+import projectsWhite from "./icons/navigation/projects-white.svg";
+import contact from "./icons/navigation/contact.svg";
+import contactWhite from "./icons/navigation/contact-white.svg";
 
-import homeWhite from "./home-white.svg";
-import aboutWhite from "./about-white.svg";
-import workWhite from "./work-white.svg";
-import projectsWhite from "./projects-white.svg";
-import contactWhite from "./contact-white.svg";
+// Social Icons
+import github from "./icons/social/github-mark.svg";
+import githubWhite from "./icons/social/github-mark-white.svg";
+import linkedin from "./icons/social/linkedin.png";
+import frontendMentor from "./icons/social/frontend-mentor.svg";
 
-import clip from "./paperclip.svg";
-import clipWhite from "./paperclip-white.svg";
+// UI Icons
+import chevronRight from "./icons/ui/chevron-right.svg";
+import chevronWhite from "./icons/ui/chevron-right-white.svg";
+import clip from "./icons/ui/paperclip.svg";
+import clipWhite from "./icons/ui/paperclip-white.svg";
 
-import github from "./github-mark.svg";
-import githubWhite from "./github-mark-white.svg";
-import linkedin from "./linkedin.png";
-import frontendMentor from "./frontend-mentor.svg";
+// Tech Icons
+import html from "./icons/tech/html.png";
+import css from "./icons/tech/css.png";
+import js from "./icons/tech/javascript.png";
+import react from "./icons/tech/reactjs.png";
+import ts from "./icons/tech/typescript.png";
+import node from "./icons/tech/nodejs.png";
+import mongo from "./icons/tech/mongodb.png";
+import redux from "./icons/tech/redux.png";
+import git from "./icons/tech/git.png";
+import figma from "./icons/tech/figma.png";
+import tailwind from "./icons/tech/tailwind.png";
+import nextjs from "./icons/tech/nextjs.svg";
 
-import chevronRight from "./chevron-right.svg";
-import chevronWhite from "./chevron-right-white.svg";
-import text from "./text.svg";
-import textWhite from "./text-white.svg";
+// Project Images
+import luminous from "./images/projects/luminous.png";
+import proompt from "./images/projects/proompt.png";
+import netflix from "./images/projects/netflix-clone.png";
+import rental from "./images/projects/car-rental.jpeg";
+import space from "./images/projects/space-tourism.jpeg";
 
-import html from "./html.png";
-import css from "./css.png";
-import js from "./javascript.png";
-import react from "./reactjs.png";
-import ts from "./typescript.png";
-import node from "./nodejs.png";
-import express from "./expressjs.png";
-import mongo from "./mongodb.png";
-import redux from "./redux.png";
-import git from "./git.png";
-import figma from "./figma.png";
-import tailwind from "./tailwind.png";
-import nextjs from "./nextjs.svg";
-import framer from "./framer-motion.png";
-
-import luminous from "./luminous.png";
-import proompt from "./proompt.png";
-import netflix from "./netflix-clone.png";
-import rental from "./car-rental.jpeg";
-import space from "./space-tourism.jpeg";
-
-import me from "./KhizarNawab.png";
-import logo from "./logo.svg";
-import logoWhite from "./logo-white.svg";
+// Profile & Branding
+import me from "./images/KhizarNawab.png";
+import logo from "./branding/logo.svg";
+import logoWhite from "./branding/logo-white.svg";
 
 export {
+  // Navigation
   home,
-  about,
-  work,
-  projects,
-  contact,
   homeWhite,
+  about,
   aboutWhite,
-  workWhite,
+  projects,
   projectsWhite,
+  contact,
   contactWhite,
+  // Social
   github,
   githubWhite,
   linkedin,
   frontendMentor,
+  // UI
   chevronRight,
   chevronWhite,
-  text,
-  textWhite,
+  clip,
+  clipWhite,
+  // Tech
   html,
   css,
   js,
   react,
   ts,
   node,
-  express,
   mongo,
   redux,
   git,
   figma,
   tailwind,
   nextjs,
-  me,
-  framer,
+  // Projects
   luminous,
   proompt,
   netflix,
   rental,
   space,
-  clip,
-  clipWhite,
+  // Profile & Branding
+  me,
   logo,
   logoWhite,
 };
