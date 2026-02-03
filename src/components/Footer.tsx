7cd2333c7c32ae687d36652fd819b8cc7fc37a24
Khizar <khizarnawab1@outlook.com>
2026-02-03 21:50:33 +0500

    fix: update copyright year and text in footer
    - Update year from 2023 to 2026
    
    - Fix typo: 'Right' to 'Rights'

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <div className="bottom-0 w-full right-0 left-0 py-4 px-10 text-center text-xs text-gray-600 dark:text-gray-400 my-20 sm:m-0 sm:mt-10">
-      2023 &copy; All Right Reserved <br /> Khizar Nawab
+      2026 &copy; All Rights Reserved <br /> Khizar Nawab
     </div>
   );
 };
